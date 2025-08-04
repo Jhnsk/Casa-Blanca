440d156c2ff89b508d59e9e5e4fdf0ee1e2fb5fa
Jhnsk <150840061+Jhnsk@users.noreply.github.com>
2025-08-04 19:12:27 -0300

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+  <meta name="theme-color"content="black" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>HTML</title>
+  <link rel="stylesheet" href="style.css">
+  <script src="https://unpkg.com/scrollreveal"></script>
+  
+</head>
+<body>
+  <header class="header">
+    <div class="text ">
+       <h1 class="hidden">Casa Blanca</h1>
+      <p class="hidden">Delicatessen</p>
+    </div>
+   
+  </header>
+  <div class="navegation">
+    <nav class="nav">
+      <li><a class="coffe active" href="#">Café</a></li>
+      <li><a class="food" href="#">Lanches</a></li>
+    </nav>
+    
+  </div>
+  <div class="first-section">
+    
+    <div class="model-comtent visivel">
+      <div class="model-item">
+        <div class="model-item-img">
+           <img src="" alt="" />
+        </div>
+        <div class="infos">
+          <div class="item-name"></div>
+          <div class="item-info"></div>
+          <div class="item-price">R$:</div>
+        </div>
+      </div>
+    </div>
+   
+   <div class="area">
+     
+   </div>
+    
+  </div>
+  <script src="coffe.js"></script>
+  <script src="food.js"></script>
+  <script src="main.js"></script>
+  <script src="reveal.js" defer></script>
+  
+</body>
+</html>
