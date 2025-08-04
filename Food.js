65cb174c3f2019d6bf867fa60a8ed82e7d0cb4b7
Jhnsk <150840061+Jhnsk@users.noreply.github.com>
2025-08-04 19:16:11 -0300

    Create Food.js

---
+++ Food.js
@@ -0,0 +1,107 @@
+let food = [
+{
+  name: 'X-Burguer',
+  info: 'Pão tostado na manteiga,queijo mussarela, carne de hambúrguer e salada',
+  price: 'R$: 12,00',
+  img:'xburguer.jpeg'
+  
+},
+{
+  name:'X-Salada',
+  info:'Pão tostado na manteiga,queijo mussarela, presunto, carne de hambúrguer e salada',
+  price: 'R$: 13,00',
+  img:'xsalada.jpeg'
+},
+{
+  name:'X-Frango',
+  info:'Pão tostado na manteiga,queijo mussarela, frango desfiado e salada',
+  price: 'R$: 13,00',
+  img:'xfrango.jpeg'
+},
+{
+  name:'X-Frango Bacon',
+  info:'Pão tostado na manteiga,queijo mussarela, frango desfiado, bacon e salada',
+  price: 'R$: 18,00',
+  img:'xfranbacon.jpeg'
+},
+{
+  name:'X-Egg',
+  info:'Pão tostado na manteiga,queijo mussarela, carne de hambúrguer, ovo e salada',
+  price: 'R$: 14,00',
+  img:'xegg.jpeg'
+},
+{
+  name:'X-Egg Calabresa',
+  info:'Pão tostado na manteiga,queijo mussarela, carne de hambúrguer, ovo, calabresa e salada',
+  price: 'R$: 17,00',
+  img:'xeggcalabresa.jpeg'
+},
+{
+  name:'X-Egg Bacon',
+  info:'Pão tostado na manteiga,queijo mussarela, carne de hambúrguer, ovo, bacon e salada',
+  price: 'R$: 17,00',
+  img:'xeggbacon.jpeg'
+},
+{
+  name: 'X-Calabresa',
+  info: 'Pão tostado na manteiga,queijo mussarela, carne de hambúrguer,calabresa e salada',
+  price: 'R$: 12,00',
+  img: 'xcalabresa.jpeg'
+},
+{
+  name: 'X-Bacon',
+  info: 'Pão tostado na manteiga,queijo mussarela, carne de hambúrguer, bacon e salada',
+  price: 'R$: 15,00',
+  img: 'xbacon.jpeg'
+},
+{
+  name: 'Hambúrguer',
+  info: 'Pão tostado na manteiga, carne de hambúrguer e salada',
+  price: 'R$: 10,00',
+  img: 'hambúrguer.jpg'
+},
+{
+  name: 'Americano',
+  info: 'Pão tostado na manteiga, carne de hambúrguer, presunto, ovo e salada',
+  price: 'R$: 11,00',
+  img: 'americano.jpeg'
+},
+{
+  name: 'X-Tudo',
+  info: 'Pão tostado na manteiga, carne de hambúrguer, presunto, ovo, frango, bacon, calabresa e salada',
+  price: 'R$: 24,00',
+  img: 'xtudo.jpeg'
+},
+{
+  name: 'Pão com Manteiga',
+  price: 'R$: 2,50',
+  img: 'paocmanteiga.jpeg',
+  info:''
+},
+{
+  name: 'Pão com Ovo',
+  price: 'R$: 5,00',
+  img: 'paocovo.jpeg',
+  info:''
+},
+{
+  name: 'Pão com Queijo',
+  price: 'R$: 7,00',
+  img: 'paocqueijo.jpeg',
+  info:''
+},
+{
+  name: 'Pão com Queijo e Presunto',
+  price: 'R$: 7,00',
+  img: 'paopreequeijo.jpeg',
+  info:''
+  
+},
+{
+  name: 'Pão com Queijo, ovo e Presunto',
+  price: 'R$: 10,00',
+  img: 'mistocovo.jpeg',
+  info:''
+},
+
+];
