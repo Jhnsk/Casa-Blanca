d2f5b94a3b209d6d953335dc58abd33428871621
Jhnsk <150840061+Jhnsk@users.noreply.github.com>
2025-08-04 19:15:12 -0300

    Update Coffe.js

--- Coffe.js
+++ Coffe.js
@@ -1,1 +1,53 @@
-
+let coffe = [
+  {name:'Expresso',
+    info:'Médio R$: 12,90 Pequeno R$: 6,90',
+    img:'expresso.jpeg',
+    price:''
+    
+  },
+  {name:'Machiato',
+    info:'Café Expresso e Leite vaporizado',
+    price:'R$: 8,90',
+    img:'machiato.jpeg'
+  },
+  {name:'Cappuccino',
+    info:'Com açúcar Sem açúcar',
+    price:'R$: 12,90',
+    img:'cappuccino.jpeg'
+  },
+  {name:'Mocha',
+    info:'Caldo de chocolate, leite vaporizado e café expresso',
+    price:'R$: 14,90',
+    img:'mocha.jpeg'
+  },
+  {name:'Cappuccino Italiano',
+    info:'Café expresso, leite vaporizado e canela em pó',
+    price:'R$: 11,90',
+    img:'capuccinoitaly.jpeg'
+  },
+  {name:'Expresso Carioca',
+    info:'Expresso e água quente',
+    price:'R$: 6,90',
+    img:'expcarioca.jpeg'
+  },
+  {name:'Café',
+    info:'Café pequeno R$: 3,00 Café Médio R$: 3,50',
+    img:'cafe.jpeg',
+    price:''
+  },
+  {name:'Chocolate Quente',
+    info:'',
+    price:'R$: 14,90',
+    img:'chocolateq.jpeg'
+  },
+  {name:'Affogato',
+    info:'Calda de chocolate, sorvete de creme e café expresso',
+    price:'R$: 17,90',
+    img:'affogato.jpeg'
+  },
+  {name:'Frappê de Capuccino',
+    info:'Café expresso, frappê, sorvete de creme e chantilly',
+    price:'R$: 17,90',
+    img:'cappuccifrappe.jpeg'
+  },
+]
